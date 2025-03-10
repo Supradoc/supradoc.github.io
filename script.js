0051d31b3723c542a4101afbe00524ba51be2216
Johannes Lyytikkä <Johannes.lyytikka@gmail.com>
2025-03-10 13:39:50 +0200

    Split into separate files

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file is currently empty as there's no JavaScript functionality
+// in the original page. It's included as a placeholder for future enhancements.
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Future JavaScript functionality can be added here
+});
